d4619591bfb04a4ad1725966a40cd726da857eb5
Nagel <pedronagelalves@gmail.com>
2025-04-01 23:01:21 -0300

    Pagina de produto na pagina inicial

--- front-ddd/src/App.js
+++ front-ddd/src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PaginaProduto from "./PaginaProduto";
+import "./PaginaProduto.css"; // Mantém os estilos do App, se quiser
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <PaginaProduto />;
 }
 
 export default App;
