b8d9275e4b64b119fea8952c52684362fa63a096
Igor Mateus Mordaski <igormordaski9@gmail.com>
2025-04-05 19:29:40 -0300

    Removendo uma classe padrão gerada pelo Framework

--- backEndDDD/obj/Debug/net8.0/backEndDDD.AssemblyInfo.cs
+++ backEndDDD/obj/Debug/net8.0/backEndDDD.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backEndDDD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aede35c483d23c2a9abbb285c354237d04c0d2e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e95e4e950336224c475e76c82fa323b09bce97a")]
 [assembly: System.Reflection.AssemblyProductAttribute("backEndDDD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backEndDDD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
